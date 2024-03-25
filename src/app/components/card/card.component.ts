e7e2f982a660a54dd992fa9148368408e50a617b
aymensellaouti <aymen.sellaouti@gmail.com>
2024-03-25 13:33:00 +0000

    end card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-
+  name = 'sellaouti';
+  firstname = 'aymen';
+  job = 'traiter';
+  path = 'rotating_card_profile3.png';
 }
